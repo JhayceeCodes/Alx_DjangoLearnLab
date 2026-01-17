70db204385ff53b9d07f76839c244e0c25a1557f
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-17 13:27:38 +0100

    feat: add __str_ methods to models in relationship app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,16 +3,28 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class Library(models.Model):
     books = models.ManyToManyField(Book)
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
