a0bd17197f438aab7a00bbf2bd808308de258f03
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-17 13:36:59 +0100

    fix: update query in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,9 @@ from .models import Book, Library, Librarian
 
 Book.objects.filter(author_id=1).all()
 
-library = Library.objects.get(name="LASU library")
+library_name = "LASU Library"
+
+library = Library.objects.get(name=library_name)
 library.books.all()
 
 Librarian.objects.filter(library_id=1).all()
