d04a898f9110a38ccf52093193cb7cffed6dd351
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 23:39:06 +0100

    ...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns =[
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='login'),
     path('signup/', views.register, name='signup'),
+    path('books/add/', views.add_book, name="add_book/"),
+    path('books/edit/<int:book_id>/', views.edit_book, name="edit_book/"),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'), 
 ]
