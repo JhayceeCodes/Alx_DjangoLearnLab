1a66c35a07ce6136460d99fd93dd120ece5350f9
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 22:47:28 +0100

    feat: implement basic auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, SignUpView
+
 
 urlpatterns =[
     path('books/', list_books),
-    path('library_detail/', LibraryDetailView.as_view())
+    path('library_detail/', LibraryDetailView.as_view()),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from .models import Library, Book
 
 def list_books(request):
@@ -17,3 +20,8 @@ class BookList(ListView):
 class LibraryDetailView(DetailView):
       model = Book
 
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/signup.html'
