77731a239fe7915fc5a6dd6850a1823ea54d4870
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 22:56:31 +0100

    fix: replace custom login view with default provided LoginView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import list_books, LibraryDetailView, SignUpView, login_view
 urlpatterns =[
     path('books/', list_books),
     path('library_detail/', LibraryDetailView.as_view()),
-    path('login/', login_view),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
