5d2eae11f8d6a71dfed34c7d83ee63f3ef3d41a5
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 23:20:52 +0100

    feat: implement permission requirements on certain views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 from .models import Library, Book
 
 def list_books(request):
@@ -56,4 +57,26 @@ def login_view(request):
         form = AuthenticationForm()
     
     return render(request, 'registration/login.html', {'form': form})
+
+def is_admin(user):
+    return user.profile.role == "admin"
+
+def is_librarian(user):
+    return user.profile.role == "librarian"
+
+def is_member(user):
+    return user.profile.role == "member"
+
+@user_passes_test(is_admin, login_url="/login")
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian, login_url="/login")
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member, login_url="/login")
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
