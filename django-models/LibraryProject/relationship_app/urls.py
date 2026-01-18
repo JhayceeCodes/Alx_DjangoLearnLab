d81e3cd3a83e253029ff0be8fa4fc8d640010854
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 22:26:42 +0100

    feat: add library_list template and rename list_books to books_list

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.book_list)
+    path('', views.book_list),
+    path('books/', views.BookList.as_view())
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book
+from .models import Book, Library
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all() 
       context = {'books': books} 
-      return render(request, 'relationship_app/list_books.html', context)
+      return render(request, 'relationship_app/books_list.html', context)
 
 
 class BookList(ListView):
-      ...
+      model = Library
+      template_name = 'books/list.html'
 
 class BookDetail(DetailView):
-      ...
+      model = Book
 
