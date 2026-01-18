c5614d2b98d6a4262fa42a3890e0d0369a2e223f
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 23:43:27 +0100

    ...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,27 +13,32 @@ from django.contrib.auth.decorators import permission_required, user_passes_test
 from .models import Library, Book
 
 
-@login_required
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
-@login_required
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-@login_required
-def is_member(user):
-    return user.userprofile.role == 'Member'
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, "relationship_app/admin_view.html")
 
+
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request, "relationship_app/librarian_view.html")
 
+
+@login_required
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request, "relationship_app/member_view.html")
 
 
 def list_books(request):
