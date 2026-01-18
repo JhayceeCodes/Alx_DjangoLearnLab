fdbdaf9f7cb262e0f8f07e8a75a6734d91625dae
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 01:04:24 +0100

    feat: implement function-based view for book list

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+      """Retrieves all books and renders a template displaying the list."""
+      books = Book.objects.all() 
+      context = {'books': books} 
+      return render(request, 'list_books.html', context)
+
+
+class BookList(ListView):
+      ...
+
+class BookDetail(DetailView):
+      ...
+
