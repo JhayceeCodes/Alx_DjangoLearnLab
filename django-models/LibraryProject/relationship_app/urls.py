4068cd6be35747514baa7063a6f3430616ed3099
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-18 22:55:11 +0100

    feat: implement custom login view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, SignUpView
+from .views import list_books, LibraryDetailView, SignUpView, login_view
 
 
 urlpatterns =[
     path('books/', list_books),
     path('library_detail/', LibraryDetailView.as_view()),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('login/', login_view),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import Library, Book
@@ -25,3 +28,24 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/signup.html'
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page (e.g., a home or dashboard view)
+                return redirect('home') 
+            else:
+                # Handle invalid login (though form.is_valid() usually catches this)
+                pass 
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'registration/login.html', {'form': form})
+
