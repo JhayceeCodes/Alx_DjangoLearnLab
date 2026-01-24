04cf44f6e73c086316fef95cdcb938284b2f1b4f
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-24 01:46:23 +0100

    feat: add custom user model in bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
