5beaadd0bc14dbbe52cdd3b7806712d71340282d
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-24 02:13:55 +0100

    fix: aadd site in @admin decorator

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Book, CustomUser, CustomUserAdmin
 
-@admin.register(Book)
+@admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
 
 
-@admin.register(CustomUser, CustomUserAdmin)
+@admin.site.register(CustomUser, CustomUserAdmin)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "date_of_birth")
