b7f784b5a7bc60a0ea075e196e58db6ec70ed0e6
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-24 01:59:34 +0100

    feat: implement custom user manager, and register in admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "date_of_birth")
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
+
+class CustomUserManager(UserManager):
+    def create_user(self, username, password=None, date_of_birth=None, profile_photo=None):
+        if not username:
+            raise ValueError("Username is required")
+        user = self.model(username=username, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, username, email, password, **extra_fields):
+        return super().create_superuser(username, email, password, **extra_fields)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
