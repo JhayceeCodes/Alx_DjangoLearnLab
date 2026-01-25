9a0b9650b2e7ff303a1a09a666b88119a59ec576
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-25 20:23:09 +0100

    feat: integrate custom permissions in bookshelf views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+#can_create --- users with this permission can add new books
+@permission_required('book_shelf.can_create', raise_exception=True)
+def create_book(request):
+    ...
+
+
+#can_list --- users with this permission can view books
+@permission_required('book_shelf.can_view', raise_exception=True)
+def list_books(request):
+    books = Book.objects.all()
+    return HttpResponse({
+        "books": books
+    })
+
+
+#can_edit --- users with this permission can edit book details
+@permission_required('book_shelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    ...
+
+
+#can_edit --- users with this permission can delete book with provided book id
+@permission_required('book_shelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    ...
