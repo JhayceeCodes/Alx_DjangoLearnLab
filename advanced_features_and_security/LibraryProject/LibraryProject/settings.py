574fcb973c08cf8f3e7b16e8c9b450895eb044b6
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-25 20:34:57 +0100

    feat: configure security settings for production, add template files in bookshelf

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -23,9 +23,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f+mq2fz=oa5*)q0ky(%0rqv8vpv)8+#u=d(u_2q$uh&ve10&d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+#PROD
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = "DENY"
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+ALLOWED_HOSTS = ["http://127.0.0.1:8000/"]
 
 
 # Application definition
