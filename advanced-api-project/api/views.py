4946a6ff7a0a3d12a4692b43151e5b3d7e08ca65
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-07 01:40:45 +0100

    feat: implement generic views to perform CRUD actions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+class BookListView(ListAPIView):
+    """Return a list of all books."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
+
+class BookDetailView(RetrieveAPIView):
+    """Return details of a single book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(CreateAPIView):
+    """Allow authenticated users to create a book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookUpdateView(UpdateAPIView):
+    """Allow authenticated users to update a book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookDestroyView(DestroyAPIView):
+    """Allow authenticated users to delete a book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
