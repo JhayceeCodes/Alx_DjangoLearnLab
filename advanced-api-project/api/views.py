eabadf791da51bbccbc0597db8411bee4d5f6d0b
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-07 01:54:31 +0100

    feat: implement read-only or authenticated permission in api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,38 +1,43 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializers import BookSerializer
 from .models import Book
 
 class BookListView(ListAPIView):
     """Return a list of all books."""
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-
+    
 
 class BookDetailView(RetrieveAPIView):
     """Return details of a single book."""
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookCreateView(CreateAPIView):
     """Allow authenticated users to create a book."""
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    
 
 
 class BookUpdateView(UpdateAPIView):
     """Allow authenticated users to update a book."""
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    
 
 
 class BookDeleteView(DestroyAPIView):
     """Allow authenticated users to delete a book."""
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    
