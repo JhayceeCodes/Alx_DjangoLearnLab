f041d8ebd5a1cf2030d544f0d49357b4d670e502
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-07 01:11:17 +0100

    feat: setup advanced api project

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField
