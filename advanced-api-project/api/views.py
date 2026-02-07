45dc71d397859518605786925c1024470a750337
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-07 01:59:30 +0100

    feat: implement filtering and ordering in api views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import BookSerializer
 from .models import Book
 
 class BookListView(ListAPIView):
     """Return a list of all books."""
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+    search_fields = ["title", "author"]
+    ordering_fields = ["title", "publication_year"]
+    ordering = ["-publication_year"]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
