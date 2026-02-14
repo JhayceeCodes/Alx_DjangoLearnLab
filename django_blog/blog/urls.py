21d1d1b4f6072602db58856734b2b5e8a326c301
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-14 16:05:39 +0100

    fix: change route for home path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import home, register_view
 
 urlpatterns =[
-    path("home/", home, name="home"),
+    path("", home, name="home"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", register_view, name="register")
