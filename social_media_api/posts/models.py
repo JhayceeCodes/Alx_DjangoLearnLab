e0bfda9bf9d51b18c105a0593944fdca756ae37e
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-22 21:30:54 +0100

    feat: add Like model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -17,3 +17,9 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
