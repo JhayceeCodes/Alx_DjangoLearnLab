b02ce96a089c1e7f95700345049cf2d97b3bcdbf
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-22 20:36:40 +0100

    Revert "refactor: remove unnecessary import"
    This reverts commit 9b57d6464b9eef42ecb05305d3fa8bf90f420efb.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,18 +2,18 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
- 
+User = get_user_model() 
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ["id", "username", "email", "password"]
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"]
