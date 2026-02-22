3206c4a58e371d2ee3e3c4148dd9ee622bcc1739
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-22 20:39:14 +0100

    refactor: restructure import

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().select_related("author")
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -14,7 +14,7 @@ class PostViewSet(ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().select_related("author", "post")
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
